4091cfa66b6c0caaa44ad0f2a8bd28b1ef016f49
cccounte <danielnasc15987@gmail.com>
2021-06-08 13:03:50 -0300

    SHOOOOOOT

---
+++ Assets/Scripts/gun/gunRotation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunRotation : MonoBehaviour
+{
+    //Components
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    //Vectors
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void FixedUpdate() {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
