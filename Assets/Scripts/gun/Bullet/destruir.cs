e44afc6898dafe4e128ca76962bcc1287a17113e
cccounte <danielnasc15987@gmail.com>
2021-06-08 13:11:34 -0300

    destroy bullet

---
+++ Assets/Scripts/gun/Bullet/destruir.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruir : MonoBehaviour
+{
+
+    public float lifeTime;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+}
