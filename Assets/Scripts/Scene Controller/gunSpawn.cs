899bbac4e202bf8b8adc683a881b7091b6bb135f
cccounte <danielnasc15987@gmail.com>
2021-06-09 14:46:02 -0300

    time to spawn gun and camera follows the player

--- Assets/Scripts/Scene Controller/gunSpawn.cs
+++ Assets/Scripts/Scene Controller/gunSpawn.cs
@@ -7,14 +7,23 @@ public class gunSpawn : MonoBehaviour
    public GameObject gun;
    public Camera cam;
 
+   //permissao para instanciar
+   public float timeToSpawn;
+   public bool canSpawn = true;
+
     void Update()
     {   
         SpawnGun();
     }
 
     void SpawnGun(){
-        if(Input.GetButtonDown("Fire2")){
+        if(Input.GetButtonDown("Fire2") && canSpawn == true){
             Instantiate(gun, new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0), Quaternion.identity);
+            canSpawn = false;
+            Invoke("canSpawnTrue", timeToSpawn);
         }
+    }
+    void canSpawnTrue(){
+         canSpawn = true;
     }
 }
