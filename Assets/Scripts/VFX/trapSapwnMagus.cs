d95c2b049bd311c5daff7acb8f508048c4150f5a
cccounte <danielnasc15987@gmail.com>
2021-06-12 11:58:50 -0300

    lights e audio placeholder

--- Assets/Scripts/VFX/trapSapwnMagus.cs
+++ Assets/Scripts/VFX/trapSapwnMagus.cs
@@ -6,18 +6,23 @@ public class trapSapwnMagus : MonoBehaviour
 {
     public GameObject magusVfx;
     public Transform magusSpawner;
+    public GameObject backMusic;
+    public GameObject backMusic2;
+    AudioSource AudioHappy;
+    AudioSource AudioSuspense;
 
-    // private void OnCollisionEnter2D(Collision2D other) {
-    //     if(other.gameObject.tag == "Player"){
-    //         Instantiate(magusVfx, magusSpawner.position, magusSpawner.rotation);
-    //         Destroy(gameObject);
-    //     }    
-    // }
+    void Start(){
+        AudioHappy = backMusic.GetComponent<AudioSource>();
+        AudioSuspense = backMusic2.GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player"){
             Instantiate(magusVfx, new Vector3(magusSpawner.position.x, magusSpawner.position.y, 0), magusSpawner.rotation);
             Destroy(gameObject);
+            AudioHappy.Stop();
+            AudioSuspense.Play();
         }  
     }
 }
